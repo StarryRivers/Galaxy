9cb1db6d33d9bf65e833ab5c509cf589b1924ac3
StarryRivers <qinjingsheng@foxmail.com>
2020-11-18 11:49:12 +0800

    Galaxy IM Version 0.1.1118.3 Commit

---
+++ app/src/main/java/com/starryriver/galaxy/viewmodel/ConversationViewModel.java
@@ -0,0 +1,20 @@
+package com.starryriver.galaxy.viewmodel;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+/**
+ * Copyright    : qinjingsheng@foxmail.com
+ * Project      : Galaxy
+ * Package      : com.starryriver.galaxy.viewmodel
+ * Author       : StarryRivers
+ * Date         : 2020/11/18 11:38
+ * Description  : 聊天页面ViewModel
+ */
+public class ConversationViewModel extends AndroidViewModel {
+    public ConversationViewModel(@NonNull Application application) {
+        super(application);
+    }
+}
